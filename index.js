896df4fb1daddff064caaab17ab0cb4fca971f0a
Aqeel Aloraibi <aqeel.aloraibi@gmail.com>
2021-05-25 07:34:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 // Your code here
-let sourceArray =[1,2,3,-9]
-const mapToNegativize=function(){
-sourceArray.map((num)=>{
-  return num *-1
-})
-
+function mapToNegativize(sourceArray){
+    return sourceArray.map(x => x * -1);
 }
-
+function mapToNoChange(sourceArray){
+    return sourceArray.map(x => x);
+}
+function mapToDouble(sourceArray){
+    return sourceArray.map(x => x*2);
+}
+function mapToSquare(sourceArray){
+    return sourceArray.map(x => x*x);
+}
+function reduceToTotal(sourceArray, startingPoint = 0){
+    const reducer = function(accumulator, currentValue){
+      return accumulator + currentValue }
+    return sourceArray.reduce(reducer, startingPoint)
+}
+function reduceToAllTrue(sourceArray){
+    const reducer = function(accumulator, currentValue){
+        if(accumulator !== true && currentValue !== true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return sourceArray.reduce(reducer, true)
+}
+function reduceToAnyTrue(sourceArray){
+    const reducer = function(accumulator, currentValue){
+        if(accumulator === true){
+            return true
+        } else {
+            return !!currentValue
+        }
+    }
+    return sourceArray.reduce(reducer, false)
+}
